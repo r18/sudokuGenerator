275e674fd952f89e52d108a4ad1845fda82672c7
r18 <twkgr424@yahoo.co.jp>
2014-01-05 19:25:21 +0900

    generate random tables

--- main.js
+++ main.js
@@ -2,45 +2,65 @@ function main(){
     console.log("loaded");
     var container = document.getElementById("container");
     var table = document.createElement("table");
-    table.setAttribute('width',500);
-    table.setAttribute('height',500);
+    table.setAttribute('width',300);
+    table.setAttribute('height',300);
     table.setAttribute('border',1);
     table.setAttribute('bordercolor',"#333333");
     table.setAttribute('cellspacing',0);
     table.setAttribute('cellpadding',5);
     
     var tb = document.createElement("tbody");
-
-    var row1 = document.createElement("tr");
-    var row2 = document.createElement("tr");
-    var row3 = document.createElement("tr");
-
-    var data1 = document.createElement("td");
-    var data2 = document.createElement("td");
-    var data3 = document.createElement("td");
-    var data4 = document.createElement("td");
-   
-
-    data4.innerHTML = "poyo";
-    data3.innerHTML = "piyo";
-    data2.innerHTML = "fuga";
-    data1.innerHTML = "hoge";
-
-    row1.appendChild(data1);
-    row1.appendChild(data2);
-    row2.appendChild(data3);
-    row2.appendChild(data4);
-    
-    tb.appendChild(row1);
-    tb.appendChild(row2);
+    var rows = [];
+    for(var i=0;i<3;i++)rows.push(document.createElement("tr"));
+    for(var i=0;i<3;i++){
+	for(var j=0;j<3;j++){
+	    rows[i].appendChild(mkData(genRandomTable().outerHTML));
+	}
+	tb.appendChild(rows[i]);
+    }
     table.appendChild(tb);
     container.appendChild(table);
+}
+function genRandomTable(){
+    var table = document.createElement("table");
+    table.setAttribute('width',100);
+    table.setAttribute('height',100);
+    table.setAttribute('border',1);
+    table.setAttribute('bordercolor',"#333333");
+    table.setAttribute('cellspacing',0);
+    table.setAttribute('cellpadding',5);
+
+    var tb = document.createElement("tbody");
+    var rows = [];
+    var r = randomSeq();
+    for(var i=0;i<3;i++)rows.push(document.createElement("tr"));
+    for(var i=0;i<3;i++){
+	for(var j=0;j<3;j++){
+	    rows[i].appendChild(mkData(r[i*3+j]));
+	}
+	tb.appendChild(rows[i]);
+    }
+    table.appendChild(tb);
+    return table;
 }
 
 function mkData(text){
     var td = document.createElement("td");
     td.innerHTML = text;
     return td;
+}
+
+function randomSeq(){
+    s = [1,2,3,4,5,6,7,8,9];
+    for(var i=0;i<50;i++){
+	s = swap(s,Math.floor(Math.random()*8));
+    }
+    return s;
+}
+
+function swap(a,n){
+    a.splice(n,2,a[n+1],a[n]);
+    return a;
 }
 
 window.onload = main;
